d07e90ceb049dc92d2c84987fd062705bec64da2
Sandy <70289658+sndyx@users.noreply.github.com>
2023-08-29 06:30:04 -0400

    Stuff

--- src/lib/util/textures.ts
+++ src/lib/util/textures.ts
@@ -1,13 +1,2 @@
 import PNG from 'pngjs';
 
-function getSkin(input: string): Uint8Array {
-    if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(input)) {
-        // Link
-        fetch(input).then((response) => {
-            response.arrayBuffer().then((buffer) => {
-                
-                new PNG.PNG().parse(buffer)
-            })
-        })
-    }
-}
